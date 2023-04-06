970333ca5529c8a863a03276cad2c6614b0d5d6e
Huyhsu <79528605+Huyhsu@users.noreply.github.com>
2023-04-06 23:05:18 +0800

    test

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/test", (req, res) => {
+  res.send("This is for test.");
+});
+
+app.listen(5500, () => {
+  console.log("Server Running");
+});
