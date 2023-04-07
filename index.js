e327958a3bc89a3df6a61588484ffc32f34417e1
Huyhsu <79528605+Huyhsu@users.noreply.github.com>
2023-04-07 11:26:43 +0800

    test ejs

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 const app = express();
 
+let engine = require("ejs-locals");
+
 app.use(express.static(__dirname + "/public"));
 
+app.engine("ejs", engine);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// app.use('/ntuedtd112', express.static(__dirname + '/public'));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.render("index.ejs");
 });
 
 app.get("/test", (req, res) => {
+  // res.send("This is for test.");
+  res.sendFile(path.join(__dirname, "index2.html"));
+});
+
+app.get("/testparam", (req, res) => {
   // res.send("This is for test.");
   res.sendFile(path.join(__dirname, "index2.html"));
 });
