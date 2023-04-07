92fe734ff9f9bfffb28fd29cbf23406c207bbdee
Huyhsu <79528605+Huyhsu@users.noreply.github.com>
2023-04-07 18:14:34 +0800

    test esj layout

--- index.js
+++ index.js
@@ -4,32 +4,40 @@ const path = require("path");
 const engine = require("ejs-locals");
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+const testJsonData = require(__dirname + "/public/json/testdata.json");
 
+// import testJsonData from
+
+// Public Folder
+app.use(express.static(__dirname + "/public"));
+// app.use('/ntuedtd112', express.static(__dirname + '/public'));
+
+// Ejs Settings
 app.engine("ejs", engine);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-// app.use('/ntuedtd112', express.static(__dirname + '/public'));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { title: "123", testdata: testJsonData });
+});
+
+app.get("/tt", (req, res) => {
+  res.render("index.ejs", { title: "87484" });
 });
 
 app.get("/test", (req, res) => {
-  // res.send("This is for test.");
   res.sendFile(path.join(__dirname, "index2.html"));
 });
 
 app.get("/testparam", (req, res) => {
-  // res.send("This is for test.");
   res.sendFile(path.join(__dirname, "index2.html"));
 });
 
+// app.get("/", (req, res) => {
+//   res.send("This is for test.");
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
 app.listen(5500, () => {
-  console.log("Server Running");
+  console.log("Server Running On Port 5500");
 });
